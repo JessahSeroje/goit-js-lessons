1a91d2ad671f141686386483982f94ec990fb61d
JessahSeroje <jseroje.actionlabs@gmail.com>
2023-10-19 20:27:17 +0800

    lesson 2 initial commit
    MOdule 1 - Lesson 2

---
+++ js/example-11.js
@@ -0,0 +1,30 @@
+/* 
+Example 1 - User Input and Branching
+
+Using the if..else and prompt constructs, write code that will ask: "What is the official name of JavaScript?". 
+If the user enters ECMAScript, then show an alert with the string "Correct!", otherwise - "Do not know? ECMAScript!"
+
+const answer = prompt('What is the official name of JavaScript?');
+const rightAnswer = 'ECMAScript';
+// Write if else code below this line
+*/
+
+const answer = prompt("What is the official name of JavaScript?");
+const rightAnswer = "ECMAScript";
+// Write if else code below this line
+
+if (answer.toLowerCase() === rightAnswer.toLowerCase()) {
+  console.log("Correct!");
+} else {
+  //   console.log("Do not know? ECMAScript!");
+  alert("Do not know? ECMAScript!");
+}
+
+// const answer = prompt("Panget ba si KC?");
+// const rightAnswer = "Yes";
+
+// if (answer.toLowerCase() === rightAnswer.toLowerCase()) {
+//   alert("TAMA!");
+// } else {
+//   alert("Bahala Ka Jarn!");
+// }
