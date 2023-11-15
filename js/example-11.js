10467d81c4e38570a081ee1e976a54081cac009b
JessahSeroje <jseroje.actionlabs@gmail.com>
2023-11-16 00:39:11 +0800

    late commit

--- js/example-11.js
+++ js/example-11.js
@@ -9,16 +9,16 @@ const rightAnswer = 'ECMAScript';
 // Write if else code below this line
 */
 
-const answer = prompt("What is the official name of JavaScript?");
-const rightAnswer = "ECMAScript";
-// Write if else code below this line
+// const answer = prompt("What is the official name of JavaScript?");
+// const rightAnswer = "ECMAScript";
+// // Write if else code below this line
 
-if (answer.toLowerCase() === rightAnswer.toLowerCase()) {
-  console.log("Correct!");
-} else {
-  //   console.log("Do not know? ECMAScript!");
-  alert("Do not know? ECMAScript!");
-}
+// if (answer.toLowerCase() === rightAnswer.toLowerCase()) {
+//   console.log("Correct!");
+// } else {
+//   //   console.log("Do not know? ECMAScript!");
+//   alert("Do not know? ECMAScript!");
+// }
 
 // const answer = prompt("Panget ba si KC?");
 // const rightAnswer = "Yes";
@@ -28,3 +28,13 @@ if (answer.toLowerCase() === rightAnswer.toLowerCase()) {
 // } else {
 //   alert("Bahala Ka Jarn!");
 // }
+//  --- FROM LMS ---
+// const isComing = confirm("Please confirm hotel reservation");
+// console.log(isComing);
+
+// const hotelName = prompt("Please enter desired hotel name");
+// console.log(hotelName);
+
+const value = prompt("Please enter a number!");
+console.log(typeof value); // "string"
+console.log(value); // "5"
